aa8b89d4dd54625193bbc3a029b3fbf675b99e77
Akshay07051994 <akshaygingle18@gmail.com>
2023-12-23 16:22:43 +0530

    sab hogaya

--- CRUD_Pro/urls.py
+++ CRUD_Pro/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app1.views import ProductViewSet
+
+router = DefaultRouter()
+router.register('product', ProductViewSet, basename='product' )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from app1.models import Product
+from app1.serializers import ProductSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
